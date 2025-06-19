c96f34acd61b4a4faddae6a677cab48f8c9767fa
Danil Bykov <moratakki.prsnl@gmail.com>
2025-06-19 13:29:14 +0300

    Delete views directory

--- views/error.php
+++
@@ -1,9 +0,0 @@
-<?php require_once __DIR__ . '/../partials/header.php'; ?>
-
-<div class="error-message">
-    <h1>Ошибка</h1>
-    <p><?= htmlspecialchars($error ?? 'Произошла неизвестная ошибка') ?></p>
-    <a href="index.php" class="btn">← На главную</a>
-</div>
-
-<?php require_once __DIR__ . '/../partials/footer.php'; ?>
